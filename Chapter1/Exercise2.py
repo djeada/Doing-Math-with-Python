cad27f4dff5285aae480bed903dbb93229e4414d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-10-05 11:57:52 +0200

    Add files via upload

---
+++ Chapter1/Exercise2.py
@@ -0,0 +1,16 @@
+'''
+Exercise 2
+Our multiplication table generator is cool, but it prints only the first
+10 multiples. Enhance the generator so that the user can specify both the
+number and up to which multiple. For example, I should be able to input that
+I want to see a table listing the first 15 multiples of 9.
+'''
+
+def mult(x, end):
+    for i in range(x,x*(end+1),x):
+        print('number: ', x, ' mult by ', int(i/x), ' is ', i)
+
+x = int(input('Enter a number: '))
+y = int(input('Enter the last multiple: '))
+
+mult(x,y)
