0fcc95461eb5ebb7e5bb1fbb02ae88fd7ed26d6d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-10-11 21:49:14 +0200

    Add files via upload

---
+++ Chapter4/Exercise3.py
@@ -0,0 +1,26 @@
+'''
+Exercise 3
+Your challenge is to write a program that's capable of finding the sum of an
+arbitrary series when you supply the nth term of the series and the number of 
+terms in it. Here's and example of how the program would work:
+Enter the nth term: a+(n-1)*d
+Enter the number of terms: 3
+3a + 3d
+In this example, the nth term supplied is that of an arithmetic progression. 
+Starting with a and d as the common difference, the number of terms up to a 
+which the sum is to be calculated is 3. The sum turns out the be 3a + 3d, which 
+agrees with the known formula for the same.
+'''
+from sympy import expand, summation, sympify, Symbol
+from sympy.core.sympify import SympifyError
+
+expr = input('Enter the nth term: ')
+
+try:
+    expr = sympify(expr)
+except SympifyError:
+    print('Invalid input')
+
+terms = input('Enter the number of terms: ')
+
+print(expand(summation(expr, (Symbol('n'),1,terms))))
---
+++ Chapter4/Exercise4.py
@@ -0,0 +1,19 @@
+'''
+Exercise 4
+For this challenge, create a function, isolve(), that will take any inequality, 
+solve it, and then return the solution.
+'''
+from sympy import sympify, solveset, S, Symbol
+from sympy.core.sympify import SympifyError
+
+expr = input('Enter your inequality ')
+
+try:
+    expr = sympify(expr)
+except SympifyError:
+    print('Invalid input')
+
+def issolve(expr):
+    return solveset(expr, Symbol('x'), S.Reals)
+
+print(issolve(expr))
