455a4f1e3b1052ba67e5c81aa4cafd5cc4eebc3d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-10-23 11:42:46 +0200

    finished chapter 5
    finished chapter 5

---
+++ Chapter5/Exercise4.py
@@ -0,0 +1,30 @@
+'''
+Exercise 4
+
+Create a list, x, consisting of the numbers [1,2,3,4]. Then, call the shuffle()
+function(), passing this list as an argument. You'll see that the numbers in x
+have been shuffled. Note, that the list is shiffled "in place." The is, the
+original order is lost.
+But what if you wanted to use this program in a card game?There, it's not
+enough to simply output the shuffled list of integers. You'll also need a way
+to map back the integers to the specific suit and rank of each card.
+'''
+
+import random
+
+def initialize_deck():
+    suits = ['clubs','diamonds','hearts','spades']
+    ranks = ['2','3','4','5','6','7','8','9','10','jack','queen','king','ace']
+    return [(suit, rank) for suit in suits for rank in ranks]
+
+def print_deck(deck):
+    for card in deck:
+        print('{0} of {1}'.format(card[0],card[1]))
+
+deck = initialize_deck()
+print('Before shuffelig: ')
+print_deck(deck)
+              
+random.shuffle(deck)
+print('After shuffeling: ')
+print_deck(deck)
---
+++ Chapter5/Exercise5.py
@@ -0,0 +1,32 @@
+'''
+Exercise 5
+
+As a part of this challenge, write a program that will find the estimated area
+of a circle, given any radius, using this approach. The program should print 
+the estimated area of the circle for three different values of the number of
+darts: 10^3, 10^5, and 10^6. Here's a sample output of the completed solution:
+    
+Radius: 2
+Area: 12.566370614359172, Estimated (1000 darts): 12.576
+Area: 12.566370614359172, Estimated (100000 darts): 12.58176
+'''
+import math
+import random
+
+def estimate_area(r, n):
+    square_area = 4*r**2
+    count = 0
+    for i in range(n):
+        p = (random.uniform(-r,r),random.uniform(-r,r))
+        if math.sqrt(p[0]**2+p[1]**2) <= r:
+            count += 1
+    return count/n * square_area
+
+darts = [10, 100, 1000,100000,1000000]
+
+print('Input radius: ')
+radius = float(input())
+print('Area calculated with PI formula : {0:.2f}, '.format(math.pi*radius**2))
+for x in darts:
+    area = estimate_area(radius, x)
+    print('Estimated ({0} darts): {1:.2f}'.format(x,area))
