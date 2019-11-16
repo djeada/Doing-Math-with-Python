daf323dc1dc1443675e6cf2f9cd9b8a2d8f22329
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-11-16 17:19:24 +0100

    It's getting intresting

---
+++ Chapter6/Exercise1.py
@@ -0,0 +1,32 @@
+'''
+Exercise 1
+Packing Circles into a Square
+In this challenge, you'll attempt a very simplified version of the "circles 
+packed into a square" problem. How many circles of radius 0.5 will fit in the 
+square preoduced by this code?
+def draw_square():
+    ax = plt.axes(xlim= (0,6), ylim = (0,6))
+    square = plt.Polygon([(1,1), (5,1), (5,5), (1,5)], fc='b',closed = False)
+    ax.add_patch(square)
+    plt.show()
+'''
+
+import matplotlib.pyplot as plt
+
+
+def draw_square():
+    return plt.Polygon([(1,1), (5,1), (5,5), (1,5)], fc='b',closed = False)
+
+def draw_circle(x,y):
+    return plt.Circle((x, y), radius = 0.5)
+
+def draw_shapes():
+    ax = plt.axes(xlim= (0,6), ylim = (0,6))
+    ax.add_patch(draw_square())
+
+    for y in range(1,5):
+        for x in range(1,5):
+            ax.add_patch(draw_circle(x+0.5,y+0.5))
+    plt.show()
+    
+draw_shapes()
---
+++ Chapter6/Exercise2.py
@@ -0,0 +1,32 @@
+'''
+Exercise 2
+Drawing the Sierpinski Triangle
+Your challenge here is to write a program that draws the Sierpinski triangle
+composed of a certain number of points specifid as input.
+'''
+
+import matplotlib.pyplot as plt
+import random
+
+def transformation(points):
+    coefficients = [0, 0.5, 1]
+    random_coeff = coefficients[random.randint(0,2)]
+    if random_coeff != 1:
+        return 0.5 *points[0] +  random_coeff, 0.5 *points[1] +  random_coeff
+    return 0.5 *points[0] +  1, 0.5 *points[1]
+    
+def draw_sierpinski(n):
+    x = [0]
+    y = [0]
+    for i in range(n):
+        temp1, temp2 = transformation((x[i], y[i]))
+        x.append(temp1)
+        y.append(temp2)
+    return x, y
+
+print('Enter the number of points in the triangle: ')
+n = int(input())
+x, y = draw_sierpinski(n)
+plt.plot(x, y, 'o')
+plt.title('Sierpinski triangle with {0} points'.format(n))
+plt.show()
